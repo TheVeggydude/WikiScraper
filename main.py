3a50f5a35752530e824318c17eba4362198dd9e2
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2022-07-31 13:29:13 +0200

    Quick sketch of computing #refs on an example page

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import wikipediaapi as wiki_api
 
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 if __name__ == '__main__':
-    pass
+
+    # Used for easily navigating and accessing Wikipedia
+    wiki = wiki_api.Wikipedia(
+        language='en',
+    )
+
+    page = wiki.page('Python_(programming_language)')
+
+    # print(page)
+
+    page_html = urlopen(page.fullurl).read().decode('utf-8')
+
+    # print(page_html)
+
+    soup = BeautifulSoup(page_html, 'html.parser')
+
+    refs = soup.find("ol", {"class": "references"}).findAll("li")
+    print(len(refs))
